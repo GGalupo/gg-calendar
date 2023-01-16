1ae9b8fb4dac5d1b584778af017734df00cf95f9
Gustavo Galupo <ggalupo2@hotmail.com>
2023-01-15 23:46:01 -0300

    style: format with ESLint

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,4 +1,5 @@
 import { getCssText } from '@ggalupo-ui/react'
+
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -6,11 +7,13 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
         />
+
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
           rel="stylesheet"
@@ -26,6 +29,7 @@ export default function Document() {
 
       <body>
         <Main />
+
         <NextScript />
       </body>
     </Html>
