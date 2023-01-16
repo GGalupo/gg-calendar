9438157bfe853ba6a9ca8a106c8b5cf14f83f43d
Gustavo Galupo <ggalupo2@hotmail.com>
2023-01-16 00:27:57 -0300

    Form validation on home page

--- src/pages/home/components/ClaimUsernameForm/ClaimUsernameForm.tsx
+++ src/pages/home/components/ClaimUsernameForm/ClaimUsernameForm.tsx
@@ -1,16 +1,54 @@
-import { Button, TextInput } from '@ggalupo-ui/react'
+import { Button, TextInput, Text } from '@ggalupo-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-import { Form } from './styles'
+import { Form, FormAnnotation } from './styles'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, 'Username must have at least 3 characters.')
+    .regex(/^([a-z\\\\-]+)$/i, 'Username must have only letters and hyphens.')
+    .transform((username) => username.toLowerCase()),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export const ClaimUsernameForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  const handleClaimUsername = (data: ClaimUsernameFormData) => {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" placeholder="Your username" />
-      <Button size="sm" type="submit">
-        Schedule now
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form onSubmit={handleSubmit(handleClaimUsername)} as="form">
+        <TextInput
+          size="sm"
+          placeholder="Type the username"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Schedule now
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Enter the username to schedule an appointment.'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
