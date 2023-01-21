78ee423d3ef40f72050691176fbb681b46d6f2b6
Gustavo Galupo <ggalupo2@hotmail.com>
2023-01-21 02:28:06 -0300

    Styles for UpdateProfile page

---
+++ src/pages/register/update-profile/styles.ts
@@ -0,0 +1,18 @@
+import { Box, styled, Text } from '@ggalupo-ui/react'
+
+export const ProfileBox = styled(Box, {
+  marginTop: '$6',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$4',
+
+  label: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '$2',
+  },
+})
+
+export const FormAnottation = styled(Text, {
+  color: '$gray200',
+})
