87f7fbcdbcc073eee9513c68a28e0ce2304ae7d9
Gustavo Galupo <ggalupo2@hotmail.com>
2023-01-21 01:35:23 -0300

    Update buildNextAuthOptions parameters types

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -1,12 +1,16 @@
-import { type NextApiRequest, type NextApiResponse } from 'next'
+import {
+  type NextPageContext,
+  type NextApiRequest,
+  type NextApiResponse,
+} from 'next'
 import { type Adapter } from 'next-auth/adapters'
 import { destroyCookie, parseCookies } from 'nookies'
 
 import { prisma } from '../prisma'
 
 export function PrismaAdapter(
-  req: NextApiRequest,
-  res: NextApiResponse,
+  req: NextApiRequest | NextPageContext['req'],
+  res: NextApiResponse | NextPageContext['res'],
 ): Adapter {
   return {
     async createUser(user) {
