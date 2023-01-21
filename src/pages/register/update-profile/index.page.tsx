72e62859bae10fd5c03dbddc1e25071ee2cf9a13
Gustavo Galupo <ggalupo2@hotmail.com>
2023-01-21 12:13:48 -0300

    UpdateProfile page

---
+++ src/pages/register/update-profile/index.page.tsx
@@ -0,0 +1,102 @@
+import {
+  Avatar,
+  Button,
+  Heading,
+  MultiStep,
+  Text,
+  TextArea,
+} from '@ggalupo-ui/react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/router'
+import { type GetServerSideProps } from 'next'
+// eslint-disable-next-line
+import { unstable_getServerSession } from 'next-auth'
+import { useSession } from 'next-auth/react'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { buildNextAuthOptions } from '../../api/auth/[...nextauth].api'
+import { api } from '../../../lib'
+import { Container, Header } from '../styles'
+
+import { FormAnottation, ProfileBox } from './styles'
+
+const updateProfileFormSchema = z.object({
+  bio: z.string(),
+})
+
+type UpdateProfileData = z.infer<typeof updateProfileFormSchema>
+
+export default function UpdateProfile() {
+  const { data: session } = useSession()
+  const router = useRouter()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<UpdateProfileData>({
+    resolver: zodResolver(updateProfileFormSchema),
+    defaultValues: {
+      bio: '',
+    },
+  })
+
+  const handleUpdateProfile = async (data: UpdateProfileData) => {
+    try {
+      await api.put('/users/update-profile', {
+        bio: data.bio,
+      })
+
+      await router.push(`/schedule/${session?.user.username}`)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Welcome to GG Call!</Heading>
+        <Text>Last step! Let people know a bit more about yourself.</Text>
+
+        <MultiStep size={4} currentStep={4} />
+      </Header>
+
+      <ProfileBox as="form" onSubmit={handleSubmit(handleUpdateProfile)}>
+        <label>
+          <Text>Profile picture</Text>
+          <Avatar src={session?.user.avatar_url} alt={session?.user.name} />
+        </label>
+
+        <label>
+          <Text size="sm">About yourself</Text>
+          <TextArea {...register('bio')} />
+          <FormAnottation size="sm">
+            This will be shown on your profile page, and can be changed any time
+            you want.
+          </FormAnottation>
+        </label>
+
+        <Button type="submit" disabled={isSubmitting}>
+          Finish register
+          <ArrowRight />
+        </Button>
+      </ProfileBox>
+    </Container>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = await unstable_getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
+  return {
+    props: {
+      session,
+    },
+  }
+}
