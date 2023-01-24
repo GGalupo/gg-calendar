84dfb3415e6831836fa642c0fe4f87fc79e481b2
Gustavo Galupo <ggalupo2@hotmail.com>
2023-01-23 21:00:19 -0300

    Create user availability API endpoint

---
+++ src/pages/api/users/[username]/availability.api.ts
@@ -0,0 +1,82 @@
+import dayjs from 'dayjs'
+import { type NextApiRequest, type NextApiResponse } from 'next'
+
+import { prisma } from '../../../../lib/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+  const { date } = req.query
+
+  if (!date) {
+    return res.status(400).json({ message: 'Date not provided.' })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(400).json({ message: 'User does not exist.' })
+  }
+
+  const referenceDate = dayjs(String(date))
+  const isPastDate = referenceDate.endOf('day').isBefore(new Date())
+
+  if (isPastDate) {
+    return res.json({ availability: [] })
+  }
+
+  const userAvailability = await prisma.userTimeInterval.findFirst({
+    where: {
+      user_id: user.id,
+      week_day: referenceDate.get('day'),
+    },
+  })
+
+  if (!userAvailability) {
+    return res.json({ availability: [] })
+  }
+
+  const { start_time_in_minutes, end_time_in_minutes } = userAvailability
+
+  //   SCHEDULINGS ARE FROM HOUR TO HOUR
+  const startHour = start_time_in_minutes / 60
+  const endHour = end_time_in_minutes / 60
+
+  const possibleTimes = Array.from(
+    { length: endHour - startHour },
+    (_el, index) => startHour + index,
+  )
+
+  const notAvailableTimes = await prisma.scheduling.findMany({
+    select: {
+      date: true,
+    },
+    where: {
+      user_id: user.id,
+      date: {
+        gte: referenceDate.set('hour', startHour).toDate(),
+        lte: referenceDate.set('hour', endHour).toDate(),
+      },
+    },
+  })
+
+  const availableTimes = possibleTimes.filter((possibleTime) => {
+    const isAlreadyScheduled = notAvailableTimes.some(
+      (notAvailableTime) => notAvailableTime.date.getHours() === possibleTime,
+    )
+
+    return !isAlreadyScheduled
+  })
+
+  return res.json({ availability: possibleTimes, availableTimes })
+}
