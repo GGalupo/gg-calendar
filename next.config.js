e8c1e6fed78475af2be1331e1dfed1c23fe9e0c8
Gustavo Galupo <ggalupo2@hotmail.com>
2023-01-28 16:25:09 -0300

    Update ESLint config on next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+    reactStrictMode: true,
+    swcMinify: true,
+    pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 }
 
 module.exports = nextConfig
